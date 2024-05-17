6e362bfe347954e2a2d61a65f3b3ca1cdc90170e
vipul.kumar <vipul.kumar@indicchain.com>
2024-05-17 12:40:31 +0530

    initial simple image picker demo

---
+++ lib/home.dart
@@ -0,0 +1,82 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  File ? _selectedImage;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: [
+          ElevatedButton(
+            onPressed: (){
+            _pickImageFromGalary();
+          },
+              child: Text(
+                'Pick Image From Galary',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+              ),
+          ),
+          ElevatedButton(
+            onPressed: (){
+              _pickImageFromCamera();
+            },
+            child: Text(
+              'Capture Image From Camera',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          SizedBox(height: 40,),
+          Container(
+            child: _selectedImage != null ? Image.file(_selectedImage!) : Text('Please select an Image'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future _pickImageFromGalary() async{
+    final returnedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if(returnedImage == null)return;
+    setState(() {
+      _selectedImage = File(returnedImage!.path);
+    });
+  }
+
+  Future _pickImageFromCamera() async{
+    final returnedImage = await ImagePicker().pickImage(source: ImageSource.camera);
+    if(returnedImage == null)return;
+    setState(() {
+      _selectedImage = File(returnedImage!.path);
+    });
+  }
+
+
+}
